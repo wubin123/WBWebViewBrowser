ed5a25d38d7234e3cc69cfaa42ca9aaf71ec1aa9
wubin123 <wubin@wubindemacbook-pro.local>
2017-06-22 14:44:42 +0800

    更新代码

--- WBWebView/WBWebViewController.m
+++ WBWebView/WBWebViewController.m
@@ -5,6 +5,12 @@
 //  Created by 吴斌 on 2017/6/3.
 //  Copyright © 2017年 吴斌. All rights reserved.
 //
+/**                 **********************************
+ *                          如有疑问欢迎交流学习          
+ *                  新浪微博:http://weibo.com/535478908  
+ *                  github:https://github.com/wubin123 
+ *                  **********************************
+ */
 
 #import "WBWebViewController.h"
 #import <WebKit/WebKit.h>
