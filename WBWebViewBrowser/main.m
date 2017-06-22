4d527cfddc25de5e13514b0558b58412654ad081
wubin123 <wubin@wubindemacbook-pro.local>
2017-06-22 13:21:35 +0800

    Initial Commit

---
+++ WBWebViewBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WBWebViewBrowser
+//
+//  Created by 吴斌 on 2017/6/22.
+//  Copyright © 2017年 吴斌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ WBWebViewBrowserTests/WBWebViewBrowserTests.m
@@ -0,0 +1,39 @@
+//
+//  WBWebViewBrowserTests.m
+//  WBWebViewBrowserTests
+//
+//  Created by 吴斌 on 2017/6/22.
+//  Copyright © 2017年 吴斌. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WBWebViewBrowserTests : XCTestCase
+
+@end
+
+@implementation WBWebViewBrowserTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
